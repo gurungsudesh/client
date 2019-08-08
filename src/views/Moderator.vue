bfc4b172794f4bc257080a2d52a0bf74ccd699f2
Sudesh Gurung <gurung.sudesh@yahoo.com>
2019-08-08 11:54:29 +0545

    Moderator view ma component hale

--- src/views/Moderator.vue
+++ src/views/Moderator.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        
+        <UserInfo/>
+        <Search/>
+        <Posts/>
     </div>
 </template>
 
 <script>
-
+import Posts from "../components/Moderator/Posts"
+import Search from '../components/Moderator/Search'
+import UserInfo from '../components/Moderator/UserInfo'
 export default {
-    name: "Moderator"
+    name: "Moderator",
+    components: {
+        UserInfo,
+        Search,
+        Posts
+    }
 }
 </script>
 
