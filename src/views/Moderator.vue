96c6f28b64c1090ea788c958a7477e18be75cdc9
Sudesh Gurung <gurung.sudesh@yahoo.com>
2019-08-09 08:44:57 +0545

    moderator.vue ma components haru halechu

--- src/views/Moderator.vue
+++ src/views/Moderator.vue
@@ -1,34 +1,8 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <div class="container">
-            <div class="m_leftside" >
-                    <div class="m_topl">
-                            <div class="row">
-                                    <div  class="col-md-auto">
-                                      <img src="bullet.jpg" >
-                                            
-                                            <h3>Username gurung</h3>
-                                            <div id="m_lcontent">
-                                            <i class="fas fa-user-check"></i>
-                                            Verified                                                
-                                               
-                                            </p>
-                                            <button id="btn" class="btn btn-success"  >Edit Profile</button>
-                                        </div>
-                                    </div>
-                            </div>    
-                    </div>
-                    
-                        
-                    
-                    </div>
-            </div>
-=======
         <UserInfo/>
         <Search/>
         <Posts/>
->>>>>>> bfc4b172794f4bc257080a2d52a0bf74ccd699f2
     </div>
 </template>
 
