e42494126f17494e2ed056ee951a92ee884c5dcb
thedhoju <prashantdhoju@gmail.com>
2019-09-24 18:33:18 +0545

    validation error ui created

--- src/components/Register/RegisterForm.vue
+++ src/components/Register/RegisterForm.vue
@@ -60,17 +60,22 @@
                     <li class="list-group-item">
                         <input type="text" class="form-control"  id="inputPassword3"  placeholder="Answer 2" v-model= "ans2" required>
                     </li>    
-
+                    <div v-if="(error==cerror1)" class="alert  alert-warning  fade show" role="alert">
+                        <strong>{{error}}</strong> 
+                    </div>
+                    
                  <li class="list-group-item" > 
                 <button type="submit" class="btn btn-success">Sign Up</button> 
                 </li>
-                
-                <li class="list-group-item"> 
-                    <router-link style="color:green ; font-weight:none; font-size: 14px;" to="/">You have an account?</router-link>
-                </li>
-                <div  v-if="error" class="alert  alert-success  fade show" role="alert">
-                        <strong>{{error}}</strong> You should been registered.
+                <div v-if="(error==cerror2)" class="alert  alert-success  fade show" role="alert" >
+                        <strong style="margin-right:10px;">Register Sucessful</strong> Click the link below 
                     </div>
+                <li class="list-group-item"> 
+                    <router-link style="color:green ; font-weight:none; font-size: 14px; " to="/">You have an account?</router-link>
+                </li>
+                
+                    
+                   
                 </ul>
                 
 
@@ -101,6 +106,8 @@ export default {
             ques2: '',
             ans1: '',
             ans2: '',
+            cerror1:'User already exists',
+            cerror2:' registered',
 
         }
     },
